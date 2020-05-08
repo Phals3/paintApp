157f8f92d79abda91dd3666943ddb67b44218103
Phals3 <mateuszborowiecki.ik@gmail.com>
2020-05-08 03:33:34 +0200

    visual boost

--- paint/static/paint/js/canvas.js
+++ paint/static/paint/js/canvas.js
@@ -1,13 +1,32 @@
 window.addEventListener('load', () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth - 10;
-    canvas.height = window.innerHeight - 100;
+
+    canvas.height = 0.70 * window.innerHeight;
+    if (window.innerWidth >= window.innerHeight) {
+        canvas.width = 0.60 * window.innerWidth;
+    } else {
+        canvas.width = 0.90 * window.innerWidth;
+    }
+
+    let customColors = {
+        "black": "#000000",
+        "yellow": "#eae22a",
+        "red": "#d2250d",
+        "green": "#34c352",
+        "blue": "#63a9e8",
+        "rubber": "#ffffff",
+    }
+
+    for (let c in customColors) {
+        document.getElementById(c).style.backgroundColor = customColors[c];
+    }
 
     //variables
     let painting = false;
     let brushSize = 10;
     let brushPos = {x: 0, y: 0};
+
 
     function getBrushPos(canvasCtx, e) {
         let x = e.type.includes("touch") ?
@@ -50,6 +69,17 @@ window.addEventListener('load', () => {
         e.preventDefault();
     }
 
+    function scaleBrush(value) {
+        if (brushSize + value >= 1 && brushSize + value <= 50) brushSize += value;
+        document.getElementById('brush-size').innerHTML = brushSize.toString();
+    }
+
+    function setColor(id) {
+        let newColor = customColors[id];
+        ctx.strokeStyle = newColor;
+    }
+
+
     //painting actions
     canvas.addEventListener('mousedown', start);
     canvas.addEventListener('mousemove', draw);
@@ -59,26 +89,24 @@ window.addEventListener('load', () => {
     canvas.addEventListener('touchmove', draw);
     canvas.addEventListener('touchend', draw);
 
+    let toPlay = new Audio('static/paint/audio/welcome.mp3');
     document.addEventListener('click', (e) => {
         let x = e.target;
-        //color buttons
-        if (x.className === 'c_buttons') ctx.strokeStyle = x.id;
-        //scale brush
-        if (x.className === 'scale_brush') {
-            if (x.id === 'plus' && brushSize < 50) brushSize++;
-            else if (x.id === 'minus' && brushSize > 1) brushSize--;
-            document.getElementById('brush_size').innerHTML = brushSize.toString();
+        if (x.id === 'tap') {
+            if (toPlay.paused) toPlay.play();
+            else if (toPlay.currentTime > 0) toPlay.pause();
         }
-        //functional buttons
-        if (x.id === 'clear') ctx.clearRect(0, 0, canvas.width, canvas.height);
-        if (x.id === 'load') {
+        if (x.className === 'c-buttons') setColor(x.id);
+        else if (x.id === 'plus') scaleBrush(1);
+        else if (x.id === 'minus') scaleBrush(-1);
+        else if (x.id === 'clear') ctx.clearRect(0, 0, canvas.width, canvas.height);
+        else if (x.id === 'load') {
             let img = new Image(canvas.width, canvas.height);
             img.onload = () => {
                 ctx.drawImage(img, 0, 0);
             }
-            img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Stick_Figure.svg/725px-Stick_Figure.svg.png";
+            img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Stick-Figure.svg/725px-Stick-Figure.svg.png";
         }
     });
 
-    document.getElementById('brush_size').innerHTML = brushSize.toString();
 });
--- paintApp/settings.py
+++ paintApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secret_key import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ub7nmnbafrx&zz@q!6w40j%q_7jsn_9anrru1i8#4v@2k5180!'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
